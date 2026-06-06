0e44cc5d95eeefa13390d8a47a5db37641a06e7f
KamvaMacanda-St10496666 <st10496666@vcconnect.edu.za>
2026-06-06 22:09:10 +0200

    Adding new  datasets

--- Analysis.sql
+++ Analysis.sql
@@ -1,8 +1,18 @@
   -- Exploring the clean dataset 
       SELECT * 
-      FROM aiai_student_impact_staging ; 
-      
-      SELECT MAX(Weekly_GenAi_Hours) 
       FROM ai_student_impact_staging ; 
-     
-     
+       
+       -- searching for max values 
+      SELECT  MAX(Skill_Retention_Score)  , MAX(Perceived_AI_Dependency)  
+      , MAX(Anxiety_Level_During_Exams)  , MAX(Tool_Diversity) 
+      FROM ai_student_impact_staging ; 
+      
+      
+      -- Searching for students with the most Ai dependency  
+     SELECT * 
+      FROM ai_student_impact_staging 
+      WHERE Tool_Diversity=  5 ; 
+      
+      
+      
+		
