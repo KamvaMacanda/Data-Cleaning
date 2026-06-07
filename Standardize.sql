17d66cd1a4c2b5f43e503adb55288b2eacba7fc9
KamvaMacanda-St10496666 <st10496666@vcconnect.edu.za>
2026-06-07 22:50:05 +0200

    Changing date from string to date

--- Standardize.sql
+++ Standardize.sql
@@ -5,6 +5,8 @@
  UPDATE layoffs_staging_2 
  SET company = TRIM(company);   
  
+ 
+ -- Updating all cypto / cryptocurrency  industry to be named crypto 
 SELECT DISTINCT industry 
 FROM layoffs_staging_2; 
   
@@ -12,4 +14,30 @@ FROM layoffs_staging_2;
 UPDATE layoffs_staging_2 
 SET industry = 'Crypto' 
 WHERE industry LIKE 'Crypto%' ; 
+ 
+ 
+ -- removing fullspots from the end of data 
+SELECT  DISTINCT country ,  TRIM( TRAILING '.' FROM country) 
+FROM layoffs_staging_2  
+ORDER BY 1 ; 
+
+UPDATE  layoffs_staging_2 
+SET country = TRIM( TRAILING '.' FROM country)  
+WHERE  country LIKE 'United States%' ; 
+ 
+SELECT `date` ,
+STR_TO_DATE  (`date` , '%d / %m / %Y' ) 
+FROM layoffs_staging_2 ; 
+
+ 
+
+
+
+
+
+
+
+
+
+
  
