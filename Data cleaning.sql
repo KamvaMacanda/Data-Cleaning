1890d8917298a6955d2366224e303392c1ab9961
KamvaMacanda-St10496666 <st10496666@vcconnect.edu.za>
2026-06-07 16:31:30 +0200

    creating staging table for data cleaing

--- Data cleaning.sql
+++ Data cleaning.sql
@@ -1,3 +1,13 @@
 -- Cleaning data and then exploring it 
 SELECT * 
 FROM layoffs ; 
+ 
+ -- Ceating duplicate table to clean data from 
+ CREATE TABLE layoffs_staging  
+ LIKE layoffs; 
+ 
+ INSERT layoffs_staging  
+ SELECT * 
+ FROM layoffs ; 
+ 
+ 
