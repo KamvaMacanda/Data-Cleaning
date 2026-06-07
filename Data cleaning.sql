4dfa0f03ad9e37245c2abd2b6026cd368bc646db
KamvaMacanda-St10496666 <st10496666@vcconnect.edu.za>
2026-06-07 17:35:06 +0200

    Creating auto geneteated staging table 2 in order to safely delete the duplicates found

--- Data cleaning.sql
+++ Data cleaning.sql
@@ -18,12 +18,37 @@ FROM layoffs ;
  WITH duplicate_valuesCTE AS  
  ( 
   SELECT *  ,
- ROW_NUMBER() OVER(PARTITION BY company , industry , total_laid_off , percentage_laid_off, `date`)AS ID_num
+ ROW_NUMBER() OVER(PARTITION BY company , industry , total_laid_off , percentage_laid_off, `date` , 
+ stage  , country  , funds_raised_millions)AS ID_num
  FROM layoffs_staging 
  )
 
 SELECT * 
 FROM duplicate_valuesCTE 
 WHERE ID_num >1 ; 
+ 
+ SELECT * 
+ FROM layoffs_staging  
+ WHERE company = "Casper" ; 
+ 
+ 
+ CREATE TABLE `layoffs_staging` (
+  `company` text,
+  `location` text,
+  `industry` text,
+  `total_laid_off` int DEFAULT NULL,
+  `percentage_laid_off` text,
+  `date` text,
+  `stage` text,
+  `country` text,
+  `funds_raised_millions` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+ 
+ 
+ 
+ 
+ 
+ 
  
  
