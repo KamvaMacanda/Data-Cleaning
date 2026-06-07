9f0e35c94169b8899163d862f842b099e8d47c04
KamvaMacanda-St10496666 <st10496666@vcconnect.edu.za>
2026-06-07 16:47:24 +0200

    view duplicates using cte's

--- Data cleaning.sql
+++ Data cleaning.sql
@@ -9,5 +9,21 @@ FROM layoffs ;
  INSERT layoffs_staging  
  SELECT * 
  FROM layoffs ; 
+  
+ SELECT *  ,
+ ROW_NUMBER () OVER (PARTITION BY company , industry , total_laid_off , percentage_laid_off, `date`)AS ID_num
+ FROM layoffs_staging ;  
+ 
+ 
+ WITH duplicate_valuesCTE AS  
+ ( 
+  SELECT *  ,
+ ROW_NUMBER() OVER(PARTITION BY company , industry , total_laid_off , percentage_laid_off, `date`)AS ID_num
+ FROM layoffs_staging 
+ )
+
+SELECT * 
+FROM duplicate_valuesCTE 
+WHERE ID_num >1 ; 
  
  
