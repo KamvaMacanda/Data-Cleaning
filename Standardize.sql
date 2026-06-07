174d6e5f19987d85646980f1bd27da77b8524841
KamvaMacanda-St10496666 <st10496666@vcconnect.edu.za>
2026-06-07 22:07:53 +0200

    Updating all cypto / cryptocurrency  industry to be named crypto

--- Standardize.sql
+++ Standardize.sql
@@ -5,3 +5,11 @@
  UPDATE layoffs_staging_2 
  SET company = TRIM(company);   
  
+SELECT DISTINCT industry 
+FROM layoffs_staging_2; 
+  
+
+UPDATE layoffs_staging_2 
+SET industry = 'Crypto' 
+WHERE industry LIKE 'Crypto%' ; 
+ 
